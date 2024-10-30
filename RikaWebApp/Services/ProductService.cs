f37db0d2e919bbac3293368db81df93829f101be
Thanakorn Saengprachum <ThanaSaengprachum@hotmail.com>
2024-10-30 14:51:00 +0100

    ProductService: GetProductsAsync Added

---
+++ RikaWebApp/Services/ProductService.cs
@@ -0,0 +1,25 @@
+﻿namespace RikaWebApp.Services;
+
+public class ProductService
+{
+    public Task<List<Product>> GetProductsAsync()
+    {
+        var products = new List<Product>
+        {
+            new Product { Id = 1, Title = "Nike T-shirt", Price = 100, Ingress = "This is an Ingress", ImageUrl = "/images/Nike-T-shirt.jpg", Category = "Nike Sportswear" },
+            new Product { Id = 2, Title = "AIR FORCE 1", Price = 200, Ingress = "This is an Ingress", ImageUrl = "/images/AIR-FORCE-1.jpg", Category = "Nike Sportswear"},
+            new Product { Id = 3, Title = "HANS T-shirt", Price = 999, Ingress = "This is an Ingress", ImageUrl = "/images/HANS-T-shirt.jpg", Category = "Hans Sportswear"}
+        };
+        return Task.FromResult(products);
+    }
+}
+
+public class Product
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Ingress { get; set; }
+    public decimal Price { get; set; }
+    public string? ImageUrl { get; set; }
+    public string? Category { get; set; }
+}
